4833352a0355d30e8389fe44d6e79bd26e52a2f6
sora27k <aor161149@gmail.com>
2025-12-09 14:14:25 +0700

    Sorawith LabSheet04

---
+++ LabSheet04/src/Lab402.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+public class Lab402 {
+    public static void main(String[] args) {
+        String id = JOptionPane.showInputDialog("Enter student-id");
+
+        while(id.length()!=10){
+            id = JOptionPane.showInputDialog("Enter student-id, again");
+        }
+        String major;
+        if (id.substring(2,5).equals("131")){
+            major = "Information Technology (IT)";
+        }
+        else if (id.substring(2,5).equals("132")) {
+            major = "Multimedia Technology (MT)";
+        }
+        else if (id.substring(2,5).equals("133")) {
+            major = "Digital Business Information Technology (BI)";
+        }
+        else if (id.substring(2,5).equals("134")) {
+            major = "Digital Technology in Mass Communication (DC)";
+        }
+        else if (id.substring(2,5).equals("135")) {
+            major = "Data Science and Data Analytics (DS)";
+        }
+        else{
+            major = "Cannot found major";
+        }
+
+        JOptionPane.showMessageDialog(null,"Student ID: "+id+
+                "\nMajor: "+major);
+
+
+    }
+}
